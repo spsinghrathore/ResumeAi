ad9e4fc796acc4ff38951b39fc9e355837bab960
SP <spsinghrathore1807@gmail.com>
2025-07-16 11:14:29 +0530

    Headline Style Changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Result from "./pages/Result"; // ✅ Make sure this is uncommented
 import Navbar from "./assets/components/Navbar";
 import Footer from "./assets/components/Footer";
+import FloatingLink from "./assets/components/FloatingLink";
 
 // Import Analytics from Vercel package
 // import { Analytics } from "@vercel/analytics";
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} /> {/* ✅ This now works */}
       </Routes>
+       <FloatingLink />
       <Footer />
       {/* Add Analytics here
       <Analytics /> */}
---
+++ src/assets/components/FloatingLink.jsx
@@ -0,0 +1,23 @@
+// FloatingLink.jsx
+export default function FloatingLink() {
+  return (
+    <div className="fixed bottom-0 right-0 z-50">
+      <a
+        href="https://yourportfolio.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2 px-5 py-2 border border-dashed border-white/40 
+                   backdrop-blur-md bg-blue-950 text-white text-sm font-medium 
+                   rounded-tl-xl rounded-br-none rounded-tr-none rounded-bl-none 
+                   "
+      >
+        <img
+          src="profile.jpeg"
+          alt="Your Name"
+          className="w-6 h-6 rounded-full object-cover transition-transform duration-300 hover:scale-110 hover:rotate-6"
+        />
+        <span className="underline underline-offset-2">By SP</span>
+      </a>
+    </div>
+  );
+}
